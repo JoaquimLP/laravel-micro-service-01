943fa31afbb29dc64838b4d80ee3b74c0131a642
JoaquimLP <joaquimlp2013@gmail.com>
2023-02-20 21:42:29 -0300

    FIx ajuste banco de dados

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('companies', CompanyController::class);
+//Route::apiResource('companies', CompanyController::class);
 Route::apiResource('categories', CategoryController::class);
 
 Route::get('/', function () {
